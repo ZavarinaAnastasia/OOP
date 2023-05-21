8c90d7d2d441c8b1c7032f9d83d97f763f8b8760
Заварина Анастасия <60615304+ZavarinaAnastasia@users.noreply.github.com>
2023-05-21 12:34:26 +0300

    Часть 1. Лабораторная работа 2

---
+++ Lab3/Lab3/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "0,5"; // Начальное значение X
+            textBox2.Text = "2,2"; // Начальное значение Y
+            textBox3.Text = "0";// Начальное значение Z
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double x = Convert.ToDouble(textBox1.Text);
+            double y = Convert.ToDouble(textBox2.Text);
+            double z = Convert.ToDouble(textBox3.Text);
+
+            textBox4.Text = "Лаб. раб. N2. Ст. гр. 22-ИБ418 Заварина А.А." + Environment.NewLine;
+            textBox4.Text += "X = " + x.ToString() + Environment.NewLine;
+            textBox4.Text += "Y = " + y.ToString() + Environment.NewLine;
+            textBox4.Text += "Z = " + z.ToString() + Environment.NewLine;
+
+            // Определение номера выбранной функции
+            int n = 0;
+            if (radioButton2.Checked) n = 1;
+            else if (radioButton3.Checked) n = 2;
+
+            // Вычисление U
+            double u;
+            switch (n)
+            {
+                case 0:
+                    if ((z - x) == 0) u = y * Math.Sin(x) * Math.Sin(x) + z;
+                    else if ((z - x) < 0) u = y * Math.Exp(Math.Sin(x)) - z;
+                    else u = y * Math.Sin(Math.Sin(x)) + z;
+                    textBox4.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    break;
+                case 1:
+                    if ((z - x) == 0) u = y * Math.Cos(x) * Math.Cos(x) + z;
+                    else if ((z - x) < 0) u = y * Math.Exp(Math.Cos(x)) - z;
+                    else u = y * Math.Sin(Math.Cos(x)) + z;
+                    textBox4.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    break;
+                case 2:
+                    if ((z - x) == 0) u = y * Math.Exp(x) * Math.Exp(x) + z;
+                    else if ((z - x) < 0) u = y * Math.Exp(Math.Exp(x)) - z;
+                    else u = y * Math.Sin(Math.Exp(x)) + z;
+                    textBox4.Text += "U = " + Convert.ToString(u) + Environment.NewLine;
+                    break;
+                default:
+                    textBox4.Text += "Решение не найдено" + Environment.NewLine;
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "Лаб. раб. N1. Ст. гр. 22-ИБ418 Заварина А.А." + Environment.NewLine;
+        }
+    }
+}
