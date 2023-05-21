d7816fa644154e86d8dc09bc0ee2e76bbf4de9c1
Заварина Анастасия <60615304+ZavarinaAnastasia@users.noreply.github.com>
2023-05-21 12:58:03 +0300

    Часть 3. Лабораторная работа 9 (Печать документов)

---
+++ labPrint/labPrint/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+namespace labPrint
+{
+    public partial class Form1 : Form
+    {
+        private string result = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void PrintPageHandler(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString(result, new Font("Arial", 14), Brushes.Black, 0, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result = textBox1.Text;
+
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.PrintPage += PrintPageHandler;
+
+            PrintDialog printDialog = new PrintDialog();
+            printDialog.Document = printDocument;
+
+            if (printDialog.ShowDialog() == DialogResult.OK)
+                printDialog.Document.Print();
+        }
+    }
+}
